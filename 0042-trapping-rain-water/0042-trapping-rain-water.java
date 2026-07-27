d3a24219e1e747fcb3db4c5558705f029734bc63
Anmol Pandey <151553079+anmolpandey2664@users.noreply.github.com>
2026-07-27 22:20:07 +0530

    Time: 1 ms (59.71%), Space: 48.7 MB (22.76%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int[] leftMax = getLeftMaxArray(height);
+        int[] rightMax = getRightMaxArray(height);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int h = Math.min(leftMax[i], rightMax[i]) - height[i];
+            sum += h;
+        }
+
+        return sum;
+    }
+
+    public int[] getLeftMaxArray(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n];
+
+        leftMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        return leftMax;
+    }
+
+    public int[] getRightMaxArray(int[] height) {
+        int n = height.length;
+        int[] rightMax = new int[n];
+
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        return rightMax;
+    }
+}
